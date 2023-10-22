ffa1260269ad93697a2b7781991087bd79368699
Chrystalio <chrystalio.k@outlook.com>
2023-10-22 20:07:48 +0700

    Set MainActivity.kt

--- app/src/main/java/com/example/praktikum3_radiocheck/MainActivity.kt
+++ app/src/main/java/com/example/praktikum3_radiocheck/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.praktikum3_radiocheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imgBtn = findViewById<ImageButton>(R.id.imgButtonAdd)
+        imgBtn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Berhasil", Toast.LENGTH_LONG).show()
+        }
     }
 }
