e1ea99352164e51b7c533f3b46188f936a18803a
Chrystalio <chrystalio.k@outlook.com>
2023-10-22 20:05:55 +0700

    Update compiledSDKVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.praktikum3_radiocheck"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.praktikum3_radiocheck"
